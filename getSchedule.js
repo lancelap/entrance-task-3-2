3a44c0b88b041e3a252c9ba9b75c641c32410ab3
Jonh Doe <lancelap@mail.ru>
2018-08-16 12:29:01 +0500

    add final version, add eslint

--- getSchedule.js
+++ getSchedule.js
@@ -7,91 +7,160 @@ const mode = {
     start: 22,
     end: 6
   }
-}
-
+};
 
 function getSchedule(data) {
-
   const sortedRates = data.rates.concat().sort((a, b) => {
     if (a.value < b.value) { return -1; }
     if (a.value > b.value) { return 1; }
-
+  
     return 0;
   });
-
   const sortedDevices = data.devices.concat().sort((a, b) => {
     if (a.power > b.power) { return -1; }
     if (a.power < b.power) { return 1; }
-
+  
     return 0;
   });
+  const hours = convertRatesToHours(sortedRates);
+  const output = {
+    'schedule': {},
+    'consumedEnergy': {
+      'value': 0,
+      'devices': {}
+    }
+  };
 
-  const arr = sortedRates.map((element) => {
-    let hoursArr = [];
-    let hour = element.from;
+  for (let i = 0; i < sortedDevices.length; i++) { 
+    const device = sortedDevices[i];
+    // получаем массив часов работы одного прибора
+    const deviceSchedule = getScheduleOfDevice(device, hours, sortedRates);
+    
+    for (let i = 0; i < deviceSchedule.length; i++) {
+      const hour = deviceSchedule[i];
+      const valueHour = hours.get(hour).value;
+      let consumption = (device.power * valueHour) / 1000;
 
-    while (hour !== element.to + 1) {
+      hours.set(hour, {
+        power: hours.get(hour).power - device.power, 
+        mode: hours.get(hour).mode, 
+        value: valueHour
+      });
+
+      if (output.schedule[hour] === undefined) {
+        output.schedule[hour] = [device.id];
+      } else {
+        output.schedule[hour].push(device.id);
+      }
+      
+      if (output.consumedEnergy.devices[device.id] === undefined) {
+        output.consumedEnergy.devices[device.id] = consumption;
+      } else {
+        const energyDevice = output.consumedEnergy.devices[device.id];
+        output.consumedEnergy.devices[device.id] = getSum(energyDevice, consumption, 4); 
+      }
+      
+      const energyValue = output.consumedEnergy.value;
+      output.consumedEnergy.value = getSum(energyValue, consumption, 4);
+    }
+  }
+
+  return output;
+}
+
+function getScheduleOfDevice(device, hours, rates) {
+  let duration = device.duration;
+  let schedule = [];
+
+  outer: for (let i = 0; i < rates.length; i++) {
+    let from = rates[i].from;
+    const to = rates[i].to;
+    
+    while (from !== to) {
+      const start = checkHour(hours.get(from), from, device, device.duration);
+
+      if (start) {
+        let durationDevice = device.duration;
+        let time = from + (device.duration - durationDevice);
+
+        while (durationDevice > 0) {
+          const isCheck = checkHour(hours.get(time), from, device, duration);
+          if (!isCheck) {
+            
+            if (time > to || time < from) {
+              continue outer;
+            }
+
+            time++;
+            if (time >= 24) { time = 0; }
+
+            durationDevice = device.duration;
+            schedule = [];
+
+            continue;
+          } else {
+            schedule.push(time);
+          }
+
+          durationDevice --;
+          time ++;
+        }
+
+        break outer;
+      }
+      from += 1;
+      if (from >= 24) { from = 0; }
+    }
+  }
+
+  return schedule;
+}
+
+function checkHour(hour, from, device, duration) {
+  if (hour.mode !== device.mode && device.mode !== undefined) {
+    return false;
+  }
+  if (hour.power < device.power) {
+    return false;
+  }
+  if (duration > 24 - from) {
+    return false;
+  }
+
+  return true;
+}
+
+function convertRatesToHours(rates) {
+  return rates.reduce((accumulator, currentRate) => {
+    let from = currentRate.from;
+    const to = currentRate.to;
+
+    while (from !== to) {
       let hourMode = null;
-      if (hour >= mode.day.start && hour <= mode.day.end) {
+      if (from >= mode.day.start && from <= mode.day.end) {
         hourMode = 'day';
       } else {
         hourMode = 'night';
       } 
 
-      hoursArr.push({
-        hour, 
+      accumulator.set(from, { 
         power: 2100, 
-        hourMode
+        mode: hourMode,
+        value: currentRate.value
       });
 
-      hour += 1;
-      if (hour >= 24 && element.to < 23) { hour = 0 }
+      from += 1;
+      if (from >= 24 && to < 23) { from = 0; }
     }
 
-    return {
-      from: element.from, 
-      to: element.to, 
-      value: element.value,  
-      hoursArr 
-    }
-  });
-
-  // кол-во ожидающих приборов
-  let pendingDevice = 6;
-  // пока они есть работает цикл
-  outer: while (pendingDevice !== 0) {
-
-    for (let i = 0; i < sortedDevices.length; i++) {
-      const device = sortedDevices[i];
-
-      // if (device.mode === 'day') { continue }
-      // console.log(device);
-      sortedDevices.splice(i, 1);
-    }
-    pendingDevice--;
-  }
-
-
-
-
-
-  sortedDevices.forEach(device => {
-    // console.log(device.duration);
-
-    for (let i = device.duration; i >= 1; i--) {
-      console.log(i);
-      
-    }
-    console.log('@@', device.duration)
-  });
-
-  console.log(arr[1].hoursArr);
-
+    return accumulator;
+  }, new Map());
 }
 
-module.exports = getSchedule;
+function getSum(x, y, precision) {
+  const pow = Math.pow(10, precision);
+  return Math.round((x + y) * pow) / pow;
+}
 
-// if (hour >= mode.day.start && hour <= mode.day.end) {
-//   const hourMode = 'day';
-// }
-// const hourMode = 'night';
+
+module.exports = getSchedule;
--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const data = require('./input.json');
 const getSchedule = require('./getSchedule');
+const fs = require('fs');
 
-getSchedule(data);
+const output = JSON.stringify(getSchedule(data), null, 2);
+
+fs.writeFile('output.json', output, 'utf8', () => {
+});
